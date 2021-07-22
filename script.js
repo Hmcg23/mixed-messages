a21b71fe05f7801223ff615d2f0c92b9e717a8ee
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2021-07-22 13:06:12 -0700

    add gretting messages and subject messages

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const Message = {
-    words: ['Hello', 'Person', 'Why', 'Are', 'Bananas', 'Ramen', 'Codecademy!', 'This is cool', 'Did', 'You', 'Do', 'This', '.']
+    greeting: ['Did you know that', 'Fun fact:', 'Heres something you probably didn\'t know:', 'Heres something fun:', 'I bet you wished you knew that', ],
+    subject: ['all animals', 'carrots', 'peanuts', 'Chicago', 'the United States', 'vegetables', 'all people', 'cows', 'humans', 'Declan', 'homework', 'school', 'computers'],
+    fact: [],
+    createMessage() {
+        let message = [];
+        for (i = 0; i < this.words.length; i++) {
+            const wordIndex = Math.floor(Math.random() * this.words.length);
+            console.log(wordIndex)
+            message.push(this.words[wordIndex])
+        }
+        return message;
+    },
 }
+
+console.log(Message.createMessage())

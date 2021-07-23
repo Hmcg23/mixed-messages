24cd88967f9dca472958e24a90764bbc6dd7a8c5
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2021-07-23 08:45:13 -0700

    remove inneficient code, will restart

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Message = {
-    greeting: ['Did you know that', 'Fun fact:', 'Heres something you probably didn\'t know:', 'Heres something fun:', 'I bet you wished you knew that', ],
+    greeting: ['Did you know that', 'Fun fact:', 'Heres something you probably didn\'t know:', 'Heres something fun:', 'I bet you wished you knew that', 'It is a scientific fact that', 'I bet you didn\'t know that'],
     subject: ['all animals', 'carrots', 'peanuts', 'Chicago', 'the United States', 'vegetables', 'all people', 'cows', 'humans', 'Declan', 'homework', 'schools', 'computers', 'fish', 'scorpions', 'a special species of snakes'],
     fact: ['can walk down stairs', 'does not know how to swim', 'knows how to swim', 'can hover in mid-air', 'have the brain of a 5 year old child', 'can drink a gallon of milk in 3 seconds', 'weigh over 300 pounds', 'favorite food is grass', 'can chug a gallon of water', 'can swim', 'can survive underwater for more than an hour', 'among us is sus'],
     punctuation: ['.', '!', '!!!', '...', '?!', '. Cool Right?', '. I think that\'s pretty cool!!', '. Awesome.']
@@ -8,14 +8,6 @@ const Message = {
 function createMessage() {
     let message = [];
     const greetingIndex = Math.floor(Math.random(1) * Message.greeting.length);
-    const subjectIndex = Math.floor(Math.random(1) * Message.subject.length);
-    const factIndex = Math.floor(Math.random(1) * Message.fact.length);
-    const punctuationIndex = Math.floor(Math.random(1) * Message.punctuation.length);
-    message.push(Message.greeting[greetingIndex]);
-    message.push(Message.subject[subjectIndex]);
-    message.push(Message.fact[factIndex]);
-    message.push(Message.punctuation[punctuationIndex]);
-    return message.join(' ');
 };
 
 console.log(createMessage())
